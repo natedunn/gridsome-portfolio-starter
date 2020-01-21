5c7d7251681082e1c513a500c4aef52df71dec85
Nate Dunn <hello@natedunn.net>
2020-01-21 13:06:38 -0600

    Add old about content

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,17 +1,61 @@
 <template>
   <Layout>
     <div class="container mx-auto">
-      <h1>Testing</h1>
+      <g-image src="@/images/me-wide.jpg" class="w-full" />
+      <div class="h-12"></div>
+      <h1 class="text-4xl font-bold tracking-wide">About</h1>
+      <div class="h-6"></div>
+      <section>
+        <p>
+          I'm Nate Dunn, a
+          <span class="font-bold">front-end web developer and designer</span>, originally from Phoenix, Arizona but now based in
+          <span
+            class="font-bold"
+          >Mexico City 🇲🇽</span>.
+        </p>
+        <p>
+          I started building for the web in 2012 (even earlier if you count editing Myspace codes). Today I work with a wide range of stacks including
+          <span
+            class="font-bold"
+          >Wordpress</span>,
+          <span class="font-bold">Laravel</span>,
+          <span class="font-bold">Vue</span>,
+          <span class="font-bold">React</span>, and more. Additionally, I've spent a lot of time working with and authoring CSS frameworks.
+        </p>
+        <p>
+          I am a avid learner, I never settle, and I am constantly experimenting. But most of all, I'm super passionate about how I can help make the web a more
+          <span
+            class="font-bold"
+          >beautiful</span>,
+          <span class="font-bold">performant</span>, and
+          <span class="font-bold">accessible</span> experience for everyone.
+        </p>
+        <p>
+          If you would like to get in touch please try my email or
+          <Link to="https://twitter.com/natedunn">Twitter</Link>. Thanks for stopping by. 👋🏻
+        </p>
+      </section>
+      <div class="h-24"></div>
     </div>
   </Layout>
 </template>
 
 <script>
-
+import Link from "@/components/Link";
 
 export default {
   metaInfo: {
     title: 'About'
   },
+
+  components: {
+    Link,
+  }
 }
 </script>
+
+<style lang="postcss">
+p {
+  @apply text-xl mb-4 leading-relaxed;
+}
+</style>
