5b45de23f1bf2734383f52d67dd7f6ba371f053d
Nate Dunn <hello@natedunn.net>
2020-01-21 14:06:58 -0600

    Add basic projects page

---
+++ src/pages/Projects.vue
@@ -0,0 +1,29 @@
+<template>
+  <Layout>
+    <div class="container mx-auto">
+      <h1>Current projects</h1>
+      <section>
+        <ul>
+          <li>
+            <Link href="https://useproper.dev">Proper</Link>
+          </li>
+          <li>
+            <Link href="https://weirdweb.dev">Weird Web</Link>
+          </li>
+          <li>
+            <Link href="https://radlist.io">Rad</Link>
+          </li>
+        </ul>
+      </section>
+    </div>
+  </Layout>
+</template>
+
+<script>
+import Link from '@/components/Link'
+export default {
+  components: {
+    Link
+  }
+}
+</script>
