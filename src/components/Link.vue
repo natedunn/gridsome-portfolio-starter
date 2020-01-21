5158a6ec7a00157db825542427cd1aca56f52212
Nate Dunn <hello@natedunn.net>
2020-01-21 13:05:59 -0600

    Create Link component

---
+++ src/components/Link.vue
@@ -0,0 +1,32 @@
+<template>
+  <a v-if="href || external" :href="href">
+    <slot>{{ text }}</slot>
+  </a>
+  <g-link v-else :to="to">
+    <slot>{{ text }}</slot>
+  </g-link>
+</template>
+
+<script>
+export default {
+  props: {
+    external: {
+      type: Boolean,
+      required: true,
+      default: false
+    },
+    to: {
+      type: [String, Object],
+      default: null,
+    },
+    href: {
+      type: String,
+      default: null,
+    },
+    text: {
+      type: String,
+      default: null,
+    },
+  }
+}
+</script>
