34a7d838137544606292cbd0f6dbf19c02b7334b
Nate Dunn <hello@natedunn.net>
2020-02-03 20:20:23 -0600

    Add global link component and update google font

--- src/main.js
+++ src/main.js
@@ -2,12 +2,14 @@
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
 import DefaultLayout from '~/layouts/Default.vue'
+import Link from '@/components/Link.vue';
 import VueScrollTo from 'vue-scrollto'
 import VueFuse from 'vue-fuse'
 
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
+  Vue.component('Link', Link)
 
   Vue.use(VueScrollTo, {
     duration: 500,
@@ -33,7 +35,7 @@ export default function (Vue, { router, head, isClient }) {
 
   head.link.push({
     rel: 'stylesheet',
-    href: 'https://fonts.googleapis.com/css?family=Nunito+Sans:400,700'
+    href: 'https://fonts.googleapis.com/css?family=Inconsolata:400,700&display=swap'
   })
 }
 
