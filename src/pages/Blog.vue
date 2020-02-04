42d17b6ad7146d8c0deb7ddc937f1152b225b2f3
Nate Dunn <hello@natedunn.net>
2020-02-03 20:20:08 -0600

    Minor clean up

--- src/pages/Blog.vue
+++ src/pages/Blog.vue
@@ -1,29 +1,25 @@
 <template>
   <Layout>
     <div class="container mx-auto py-16">
-      <div
-        v-for="post in $page.posts.edges"
-        :key="post.id"
-        class="post border-gray-400 border-b mb-12"
-      >
-        <h2 class="text-3xl font-bold">
-          <g-link :to="post.node.path" class="text-primary">{{ post.node.title }}</g-link>
-        </h2>
-        <div class="text-secondary mb-4">
-          <span>{{ post.node.date }}</span>
-          <span>&middot;</span>
-          <span>{{ post.node.timeToRead }} min read</span>
-        </div>
-
-        <div class="text-lg mb-4 text-primary">{{ post.node.summary }}</div>
-
-        <div class="mb-8">
-          <g-link :to="post.node.path" class="font-bold uppercase">Read More</g-link>
-        </div>
+      <div class="border-b border-primary pb-2 flex justify-between items-center">
+        <h1 class="text-4xl font-bold font-mono">Writings</h1>
+        <Link href="/rss.xml">RSS</Link>
       </div>
-      <!-- end post -->
+      <section class="mt-6">
+        <div v-for="post in $page.posts.edges" :key="post.id" class="post border-primary border-b">
+          <div class="flex justify-between items-center">
+            <h2 class="text-2xl font-mono">
+              <Link class="inline-block py-4" :to="post.node.path">{{ post.node.title }}</Link>
+            </h2>
+            <div class="text-secondary">
+              <span>{{ post.node.date }}</span>
+            </div>
+          </div>
+        </div>
+      </section>
 
       <pagination-posts
+        class="mt-4"
         v-if="$page.posts.pageInfo.totalPages > 1"
         base="/blog"
         :totalPages="$page.posts.pageInfo.totalPages"
@@ -48,7 +44,6 @@
           title
           date(format: "MMMM D, Y")
           summary
-          timeToRead
           path
         }
       }
--- src/templates/Post.vue
+++ src/templates/Post.vue
@@ -1,7 +1,7 @@
 <template>
   <Layout>
     <div class="container mx-auto my-16">
-      <h1 class="text-4xl font-bold leading-tight">{{ $page.post.title }}</h1>
+      <h1 class="text-4xl font-bold font-mono">{{ $page.post.title }}</h1>
       <div class="text-xl text-gray-600 mb-4">{{ $page.post.date }}</div>
       <div class="flex mb-8 text-sm">
         <g-link
