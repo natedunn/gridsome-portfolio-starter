8aff187a4d342103e00a9cc20c6910ac7947d920
Nate Dunn <hello@natedunn.net>
2020-02-05 20:15:08 -0600

    Test new meta data

--- src/templates/Post.vue
+++ src/templates/Post.vue
@@ -6,7 +6,9 @@
       <div class="markdown-body mb-8" v-html="$page.post.content" />
       <div class="flex mb-8 text-sm tags">
         <span class="inline-block mr-4">Filed under:</span>
-        <Link v-for="tag in $page.post.tags" :key="tag.id" :to="tag.path">{{ tag.title }}</Link>
+        <Link v-for="tag in $page.post.tags" :key="tag.id" :to="tag.path">{{
+          tag.title
+        }}</Link>
       </div>
       <div class="mb-8">
         <Link to="/blog" class="font-bold uppercase">Back to Blog</Link>
@@ -31,35 +33,75 @@
 </page-query>
 
 <script>
+import moment from "moment";
+import slugify from "@sindresorhus/slugify";
 export default {
   metaInfo() {
     return {
       title: this.pageTitle,
       meta: [
         {
-          property: "og:image",
-          content: this.ogImageUrl
+          key: "description",
+          name: "description",
+          content: this.description(this.$page.post)
         },
+        // Open Graph
+        { property: "og:type", content: "article" },
+        { property: "og:title", content: this.$page.post.title },
         {
-          property: "og:title",
-          content: this.pageTitle
+          property: "og:description",
+          content: this.description(this.$page.post)
         },
+        { property: "og:url", content: this.postUrl },
         {
-          property: "title",
-          content: this.pageTitle
+          property: "article:published_time",
+          content: moment(this.$page.post.date).format("YYYY-MM-DD")
         },
-      ],
+        { property: "og:image", content: this.ogImageUrl },
+        // Twitter
+        { name: "twitter:card", content: "summary_large_image" },
+        { name: "twitter:title", content: this.$page.post.title },
+        {
+          name: "twitter:description",
+          content: this.description(this.$page.post)
+        },
+        { name: "twitter:site", content: "@cossssmin" },
+        { name: "twitter:creator", content: "@cossssmin" },
+        { name: "twitter:image", content: this.ogImageUrl }
+      ]
+    };
+  },
+  methods: {
+    description(post, length, clamp) {
+      if (post.description) {
+        return post.description;
+      }
+      length = length || 280;
+      clamp = clamp || " ...";
+      let text = post.content
+        .replace(/<pre(.|\n)*?<\/pre>/gm, "")
+        .replace(/<[^>]+>/gm, "");
+      return text.length > length ? `${text.slice(0, length)}${clamp}` : text;
+    },
+    titleCase(str) {
+      return str
+        .replace("-", " ")
+        .split(" ")
+        .map(s => s.charAt(0).toUpperCase() + s.substring(1))
+        .join(" ");
     }
   },
   computed: {
-    ogImageUrl () {
-      return this.$page.post.cover ? this.$page.post.cover.src : `${this.config.siteUrl}/images/default-cover.png`
+    ogImageUrl() {
+      return this.$page.post.cover
+        ? this.$page.post.cover.src
+        : `${this.config.siteUrl}/images/default-cover.png`;
     },
     pageTitle() {
       return this.$page.post.title ? this.$page.post.title : null;
     }
   }
-}
+};
 </script>
 
 <style lang="postcss">
