436aabea37ea5dbdf067b83b2f68ec6fe7836313
Nate Dunn <hello@natedunn.net>
2020-02-11 13:08:11 -0600

    Fix issue with disabled link styling

--- src/components/PaginationPosts.vue
+++ src/components/PaginationPosts.vue
@@ -1,23 +1,30 @@
 <template>
-  <div class="flex justify-between text-xl items-center">
+  <div class="flex justify-between items-center">
     <Link
       :to="previousPage"
-      :class="{ 'text-gray-400 hover:text-gray-400 cursor-not-allowed': !showPreviousPage }"
-    >&larr; Prev</Link>
-    <div class="text-base">Page {{ currentPage }} of {{ totalPages }}</div>
+      :underline="!showPreviousPage ? false : true"
+      :class="{
+        'text-gray-400 hover:text-gray-400 cursor-not-allowed': !showPreviousPage
+      }"
+      >&larr; Prev</Link
+    >
+    <div>Page {{ currentPage }} of {{ totalPages }}</div>
     <Link
       :to="nextPage"
-      :class="{ 'text-gray-400 hover:text-gray-400 cursor-not-allowed': !showNextPage }"
-    >Next &rarr;</Link>
+      :class="{
+        'text-gray-400 hover:text-gray-400 cursor-not-allowed': !showNextPage
+      }"
+      >Next &rarr;</Link
+    >
   </div>
 </template>
 
 <script>
 export default {
-  props: ['base', 'totalPages', 'currentPage'],
+  props: ["base", "totalPages", "currentPage"],
   computed: {
     showPreviousPage() {
-      return this.currentPage !== 1
+      return this.currentPage !== 1;
     },
     previousPage() {
       return [0, 1].includes(this.currentPage - 1)
@@ -25,7 +32,7 @@ export default {
         : `${this.base}/${this.currentPage - 1}`;
     },
     showNextPage() {
-      return this.currentPage !== this.totalPages
+      return this.currentPage !== this.totalPages;
     },
     nextPage(currentPage, totalPages) {
       return this.totalPages > this.currentPage
@@ -33,5 +40,5 @@ export default {
         : `${this.base}/${this.currentPage}`;
     }
   }
-}
+};
 </script>
