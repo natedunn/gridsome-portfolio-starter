39b43dfaa1fdc165fbdf1fc1fba711ab5c8c12a0
Nate Dunn <hello@natedunn.net>
2020-02-13 12:08:14 -0600

    Add to dictionary

--- src/main.js
+++ src/main.js
@@ -21,17 +21,17 @@ export default function (Vue, { router, head, isClient }) {
 
   head.meta.push({
     name: 'keywords',
-    content: 'Gridsome,Vue,Tailwind,Tailwind CSS,JavaScript,HTML,CSS,Vue.js,VueJS'
+    content: 'Nate,Dunn,Vue,Tailwind,Tailwind CSS,JavaScript,HTML,CSS,Vue.js,VueJS'
   })
 
   head.meta.push({
     name: 'description',
-    content: 'Gridsome Portfolio Starter'
+    content: 'Nate Dunn (@natedunn)'
   })
 
   head.meta.push({
     name: 'author',
-    content: 'Andre Madarang'
+    content: 'Nate Dunn'
   })
 
   head.link.push({
